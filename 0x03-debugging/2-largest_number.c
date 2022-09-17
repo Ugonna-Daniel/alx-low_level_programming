f0072a000a37ab65aebc011f207e13b47cdf4b38
ugonna-daniel <ugonnadaniel96@gmail.com>
2022-09-16 18:33:54 -0700

    initial commit

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -9,14 +9,13 @@
  */
 
 int largest_number(int a, int b, int c)
-{
-int largest;
+{int largest;
 
-if (a >= b && b >= c)
+if (a > b && a > c)
 {
 largest = a;
 }
-else if (b >= a && a >= c)
+else if (b > c)
 {
 largest = b;
 }
